ecc93fa9f095e44ba36dbc1b9a6189f4b756aa48
jolth <jolthgs@gmail.com>
2017-04-27 15:00:57 -0500

    upload POST

--- app.py
+++ app.py
@@ -16,7 +16,15 @@ class Upload:
         return render.upload()
 
     def POST(self):
-        pass
+        x = web.input(myfile={})
+        filedir = './images'
+        if 'myfile' in x:
+            filepath=x.myfile.filename.replace('\\','/')
+            filename=filepath.split('/')[-1]
+            fout = open(filedir +'/'+ filename,'w')
+            fout.write(x.myfile.file.read())
+            fout.close()
+        raise web.seeother('/upload')
 
 
 if __name__ == "__main__":
