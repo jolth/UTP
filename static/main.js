48f06aa72ddb5426759a75eabb376d4e23b0dd62
jolth <jolthgs@gmail.com>
2017-06-14 17:25:44 -0500

    verify that it is a file jpg

--- static/main.js
+++ static/main.js
@@ -1,10 +1,26 @@
 $(document).ready( function() {
+    //var _validFileExtensions = [".jpg", ".jpeg"];
+    var reg = /\.(jpg|jpeg)/g;
+
     $(document).on('change', '.btn-file :file', function() {
 	    var input = $(this),
 			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+            //console.log(input[0].type == 'file');
+            //console.log(input)
+            //console.log(label);
+            //'jorge.jpeg'.match(/\.(jpg|jpeg)/g)
+            if (input[0].type == 'file') {
+                if (!label.match(reg)) {
+                    alert("No es un jpg/jpeg");
+                    return; 
+                }
+            } else {
+                return;
+            }
 		input.trigger('fileselect', [label]);
     });
 
+    //Set file name
 	$('.btn-file :file').on('fileselect', function(event, label) {
         var input = $(this).parents('.input-group').find(':text'),
 		    log = label;
